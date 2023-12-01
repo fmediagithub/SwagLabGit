a256116ef3a8527609be50db525b414e51c93361
Mohamed.Fahiz <Mohamed.Fahiz@MFAHIZ01.corp.neudesic.net>
2023-12-01 18:34:41 +0530

    j

--- src/test/java/com/automation/businesscomponents/SetUp.java
+++ src/test/java/com/automation/businesscomponents/SetUp.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
@@ -14,8 +15,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class SetUp {
 	public static WebDriver driver;
 	public static WebDriverWait wait;
+	public static ChromeOptions options;
 	@Test(priority = 1)
 	public static void setUpBrowser() {
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--ignore-ssl-errors=yes");
+		options.addArguments("--ignore-certificate-errors");
+		
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 
